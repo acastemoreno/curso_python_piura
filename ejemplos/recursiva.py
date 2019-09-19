a395170c748e844a50b54acb2c1ccf77aa77d070
acastemoreno <acastemoreno@gmail.com>
2019-09-18 19:32:52 -0500

    funciones recursivas

--- ejemplos/recursiva.py
+++ ejemplos/recursiva.py
@@ -2,15 +2,15 @@
 def cuenta_regresiva(numero):
     numero -= 1
     if numero > 0:
-        print numero
+        print(numero)
         cuenta_regresiva(numero)
     else:
-        print "Boooooooom!"
-        print "Fin de la función", numero
+        print("Boooooooom!")
+    print("Fin de la función" + str(numero))
 
 def factorial(numero):
-    print "Valor inicial ->",numero
+    print("Valor inicial ->",numero)
     if numero > 1:
         numero = numero * factorial(numero -1)
-    print "valor final ->",numero
+    print("valor final ->" + str(numero))
     return numero
